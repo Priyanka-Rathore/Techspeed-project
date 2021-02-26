3a0c213fc336fedcd0d205fa1ce9bf8d4cb3653c
Priyanka <priyankarathore467@gmail.com>
2021-02-26 11:17:38 +0530

    first

--- Tech_Speed/settings.py
+++ Tech_Speed/settings.py
@@ -15,7 +15,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_DIR= os.path.join(BASE_DIR, 'static')
+TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
+STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'Tech_Speed.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,15 +133,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[STATIC_DIR,]
-
-STATIC_ROOT = 'staticfiles'
-
-
-
+MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/media/'
 PAYPAL_RECEIVER_EMAIL='sahooc300@gmail.com'
 PAYPAL_TEST=True
 
